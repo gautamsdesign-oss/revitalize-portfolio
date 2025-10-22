6a7739a2ed766152bb08740b161b23db1916d7c8
Gautam Sujith <gautams.design@gmail.com>
2025-10-22 08:51:56 +0100

    Save working portfolio before adding Redux

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: "/revitalize-portfolio/",
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
@@ -53,9 +54,9 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
-    server: {
+    server: { 
       port: 3000,
       open: true,
     },
